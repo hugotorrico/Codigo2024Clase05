072c5ec49563654aab2ffdd4ee78458040e84fd5
Hugo Torrico <torricomarquezh@gmail.com>
2024-09-11 19:14:12 -0500

    Agregar archivos de proyecto.

---
+++ Codigo2024Clase05/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+
+
+Console.WriteLine(int.Parse(null));//Retorna una excepción
+Console.WriteLine( Convert.ToInt32(null));//Retorna cero, controla la excepción
+
+
+
+
+Console.WriteLine("Hello, World!");
