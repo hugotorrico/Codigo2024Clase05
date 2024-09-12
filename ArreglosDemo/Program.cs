0548e3d1428bfc40f693bf2a75cb1ec5da7456c9
Hugo Torrico <torricomarquezh@gmail.com>
2024-09-11 20:30:06 -0500

    CalcularMayorNotas Arreglo

---
+++ ArreglosDemo/Program.cs
@@ -0,0 +1,95 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+void NotasSinArreglo()
+{
+    int nota1;
+    int nota2;
+    int nota3;
+    int nota4;
+    int nota5;
+    int nota6;
+    int nota7;
+    int nota8;
+    int nota9;
+    int nota10;
+
+    nota1 = 16;
+    nota2 = 18;
+    nota3 = 20;
+    nota4 = 14;
+    nota5 = 16;
+    nota6 = 18;
+    nota7 = 20;
+    nota8 = 14;
+    nota9 = 20;
+    nota10 = 14;
+
+    Console.WriteLine(nota1);
+    Console.WriteLine(nota2);
+    Console.WriteLine(nota3);
+    Console.WriteLine(nota4);
+    Console.WriteLine(nota5);
+    Console.WriteLine(nota6);
+    Console.WriteLine(nota7);
+    Console.WriteLine(nota8);
+    Console.WriteLine(nota9);
+    Console.WriteLine(nota10);
+
+}
+
+void NotasArreglos()
+{
+    int[] notas = new int[10];
+    notas[0] = 16;
+    notas[1] = 18;
+    notas[2] = 20;
+    notas[3] = 14;
+    notas[4] = 16;
+    notas[5] = 18;
+    notas[6] = 20;
+    notas[7] = 14;
+    notas[8] = 20;
+    notas[9] = 14;
+
+    for (int i = 0; i < notas.Length; i++)
+    {
+        Console.WriteLine(notas[i]);
+    }
+
+
+}
+
+int CalcularMayorNotas(int[] notas)
+{
+    int mayor = notas[0];
+
+    for (int i = 0; i < notas.Length; i++)
+    {
+        if (notas[i] > mayor)
+            mayor = notas[i];
+    }
+    return mayor;
+
+}
+
+//int mayor = notas.Max();//Función es propia del .NET
+
+int[] notasMatematica = { 16, 18, 14, 20 };
+int[] notasLenguaje = { 16, 18, 14 };
+int[] notasHistoria = { 18, 14 };
+//Matemática
+Console.WriteLine(CalcularMayorNotas(notasMatematica));
+//Lenguaje
+Console.WriteLine(CalcularMayorNotas(notasLenguaje));
+//Historia
+Console.WriteLine(CalcularMayorNotas(notasHistoria));
+
+
+Console.ReadLine();
+
+
+
+
+
+
--- Codigo2024Clase05/Program.cs
+++ Codigo2024Clase05/Program.cs
@@ -1,7 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-
-
+﻿
 void LeerNota()
 {
     int nota = 0;
@@ -19,36 +16,110 @@ void LeerNota()
 
     Console.WriteLine("Has ingresado nota: " + nota);
 }
-
-//LeerNota();
-
-string opcion;
-
-do
+void EjemploMenu()
 {
-    Console.WriteLine("Ingresa una de las siguientes opciones");
-    Console.WriteLine("A) Mensaje de Bievenida");
-    Console.WriteLine("B) Mensaje de Desepedida");
-    Console.WriteLine("S) Salir");
-    opcion = Console.ReadLine().ToUpper();
-    switch (opcion)
+    string opcion;
+
+    do
     {
-        case "A":
-            Console.WriteLine("Ud. es Bienvenido");
-            break;
+        Console.WriteLine("Ingresa una de las siguientes opciones");
+        Console.WriteLine("A) Mensaje de Bievenida");
+        Console.WriteLine("B) Mensaje de Desepedida");
+        Console.WriteLine("S) Salir");
+        opcion = Console.ReadLine().ToUpper();
 
-        case "B":
-            Console.WriteLine("Buenas noches hasta mañana");
-            break;
 
-        default:
-            break;
-    }
+        switch (opcion)
+        {
+            case "A":
+                Console.WriteLine("Ud. es Bienvenido");
+                break;
 
-    Console.WriteLine("==============================");
-    Console.WriteLine("==============================");
+            case "B":
+                Console.WriteLine("Buenas noches hasta mañana");
+                break;
 
-} while (opcion!="S");
+            default:
+                break;
+        }
+
+        Console.WriteLine("==============================");
+        Console.WriteLine("==============================");
+
+    } while (opcion != "S");
+
+
+}
+
+int CalcularMayorde4Notas(int nota1,int nota2, int nota3,int nota4)
+{
+    int mayor = nota1;    
+    if (nota2 > mayor)
+        mayor = nota2;
+    if (nota3 > mayor)
+        mayor = nota3;
+    if (nota4 > mayor)
+        mayor = nota4;
+    return mayor;
+}
+
+int CalcularMayorde3Notas(int nota1, int nota2, int nota3)
+{
+    int mayor = nota1;
+    if (nota2 > mayor)
+        mayor = nota2;
+    if (nota3 > mayor)
+        mayor = nota3;
+    if (nota3 > mayor)
+        mayor = nota3;
+    return mayor;
+}
+int CalcularMayorde2Notas(int nota1, int nota2)
+{
+    int mayor = nota1;
+    if (nota2 > mayor)
+        mayor = nota2;    
+    return mayor;
+}
+
+
+//Matemática
+Console.WriteLine(CalcularMayorde4Notas(16,18,14,20));
+Console.WriteLine(CalcularMayorde4Notas(16, 15, 14, 10));
+Console.WriteLine(CalcularMayorde4Notas(16, 17, 18, 12));
+
+//Lenguaje
+Console.WriteLine(CalcularMayorde3Notas(16, 18, 14));
+Console.WriteLine(CalcularMayorde3Notas(16, 15, 14));
+Console.WriteLine(CalcularMayorde3Notas(16, 17, 18));
+
+//Historia
+Console.WriteLine(CalcularMayorde2Notas(16, 18));
+Console.WriteLine(CalcularMayorde2Notas(16, 15));
+Console.WriteLine(CalcularMayorde2Notas(16, 17));
+
+
+
+
+
+//int nota1;
+//int nota2;
+//int nota3;
+//int nota4;
+//int mayor;
+
+//Console.WriteLine("Ingrese la nota 1");
+//nota1 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Ingrese la nota 2");
+//nota2 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Ingrese la nota 3");
+//nota3 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Ingrese la nota 3");
+//nota4 = Convert.ToInt32(Console.ReadLine());
+
+//Calcular la nota mayor
+//Console.WriteLine("La nota mayor es:");
+//Console.WriteLine( CalcularMayorde3Notas(nota1,nota2,nota3,nota4));
 
 
 
